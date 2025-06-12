6f90c0cbe4fe6e1c0c6e9bffe69b80a2e82d78b7
Sebcas16131 <sebascastellanos18131@gmail.com>
2025-06-12 08:37:34 -0500

    conexion_database

--- database/db.php
+++ database/db.php
@@ -1,4 +1,7 @@
 <?php 
 // conexion a la base de datos
-
+$conexion = new mysqli('localhost', 'root', '', 'tienda_sena');
+if ($conexion->connect_error) {
+    die("Connection failed: " . $conexion->connect_error);
+}
 ?>
