6ba6247415feb3070bc1efa958e9a5644d2ae3b7
Joalregra <johanalexis808@gmail.com>
2025-06-30 17:39:42 -0500

    Implementacion de estructura de carpetas para el trabajo en el proyecto

--- includes/db.php
+++
@@ -1,7 +0,0 @@
-<?php 
-// conexion a la base de datos
-$conexion = new mysqli('localhost', 'root', '', 'tienda_sena');
-if ($conexion->connect_error) {
-    die("Connection failed: " . $conexion->connect_error);
-}
-?>
